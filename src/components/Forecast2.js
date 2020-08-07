02f0dd46ec31c801dc008a8306294936b24035b4
wileyJak3 <wileyJak3@gmail.com>
2020-08-06 22:32:03 -0400

    forecast2 imports

---
+++ src/components/Forecast2.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DayCard from "./DayCard.js";
+import Row from "react-bootstrap/Row";
+
+
+
+export default Forecast;
