184449eab0f5520d363baa3e63beefa440fc38b0
wileyJak3 <wileyJak3@gmail.com>
2020-08-06 22:32:45 -0400

    forecast2 imports p2

--- src/components/Forecast2.js
+++ src/components/Forecast2.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DayCard from "./DayCard.js";
 import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import "../styles/Forecast2.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import PrintHour from "./PrintHour";
+import Forecast2 from "./Forecast2";
 
 
 
