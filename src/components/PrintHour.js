ef46a059db8b74d7845ff62d7c85d20a5c41e8e7
wileyJak3 <wileyJak3@gmail.com>
2020-08-08 14:14:32 -0400

    PrintHour weekday array

--- src/components/PrintHour.js
+++ src/components/PrintHour.js
@@ -3,3 +3,19 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import DayCard from "./DayCard.js";
 
+function PrintHour(props) {
+
+    console.log(props.hourly)
+
+
+    let weekday = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+
+
