6e86328f87e8fe5bd795bae93e6525130784d7d8
wileyJak3 <wileyJak3@gmail.com>
2020-08-08 14:05:49 -0400

    DayCard imports

---
+++ src/components/DayCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+//import ReactDOM from 'react-dom'
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import "../styles/DayCard.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from 
+"react-router-dom";
+
