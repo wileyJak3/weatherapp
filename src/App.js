f4c99ffc50496d5ac2131298543abbbcf83aa721
wileyJak3 <wileyJak3@gmail.com>
2020-08-08 14:39:08 -0400

    changes to git ignore and app.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import './App.css';
 import Forcast2 from './components/Forecast2'
 
 import DayCard from './components/DayCard.js';
-import Hooks from './components/hooksAPiBoilerPlate'
 function App() {
   return (
     <div>
     
     <Forcast2/>
      {/* <DayCard/> */}
-     {/* <Hooks/> */}
      
      </div>
  
