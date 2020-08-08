b4d89834be5a081d7caf68da3e21eb43a55c72ab
wileyJak3 <wileyJak3@gmail.com>
2020-08-08 14:41:32 -0400

    css change to app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Forcast2 from './components/Forecast2'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import DayCard from './components/DayCard.js';
 function App() {
   return (
