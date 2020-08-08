c2d8d3427d82c9a64b027632f557839e0019df47
wileyJak3 <wileyJak3@gmail.com>
2020-08-08 14:33:07 -0400

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Forcast2 from './components/Forecast2'
 
+import DayCard from './components/DayCard.js';
+import Hooks from './components/hooksAPiBoilerPlate'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    
+    <Forcast2/>
+     {/* <DayCard/> */}
+     {/* <Hooks/> */}
+     
+     </div>
+ 
   );
 }
 
